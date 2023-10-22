4bd3974145c0453ba0cbdd3780c234e0534e9c25
Ivan <95242947+Ims2002@users.noreply.github.com>
2023-10-22 17:23:13 +0200

    Add files via upload

---
+++ EsquemaLenguajesFreeplane/Tipos de Lenguajes.mm
@@ -0,0 +1,163 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Tipos de Lenguajes" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1697731015582"><hook NAME="MapStyle" background="#2e3440" zoom="0.455">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/dark_nord_template.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_671184412" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#eceff4" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#88c0d0" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_671184412" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" BORDER_WIDTH="1.9 px">
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ebcb8b">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#484747">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#5e81ac"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_779275544" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_779275544"/>
+<font SIZE="14"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#eceff4">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="Imperativos" POSITION="bottom_or_right" ID="ID_642697624" CREATED="1697731019592" MODIFIED="1697731030460">
+<node TEXT="Orientación a Objetos" ID="ID_664188611" CREATED="1697731081286" MODIFIED="1697731096646">
+<node TEXT="Java" ID="ID_1813698471" CREATED="1697732656042" MODIFIED="1697732660998">
+<node TEXT="Entre otros usos es utilizado para codificar aplicaciones web." ID="ID_1510876072" CREATED="1697734590966" MODIFIED="1697734670882">
+<node TEXT="IntelliJ IDEA" ID="ID_715312361" CREATED="1697734673521" MODIFIED="1697734684260"/>
+<node TEXT="NetBeans" ID="ID_514934197" CREATED="1697734684711" MODIFIED="1697734690719"/>
+<node TEXT="Eclipse" ID="ID_524383023" CREATED="1697734690875" MODIFIED="1697734698479"/>
+</node>
+</node>
+<node TEXT="C++" ID="ID_1339286328" CREATED="1697732661858" MODIFIED="1697732734815">
+<node TEXT="Creación de programas como controladores, juegos, aplicaciones móviles..." ID="ID_1470471019" CREATED="1697734720023" MODIFIED="1697734751761">
+<node TEXT="C++ Builder" ID="ID_118384136" CREATED="1697734754022" MODIFIED="1697734785693"/>
+<node TEXT="CodeLite" ID="ID_285180877" CREATED="1697734785835" MODIFIED="1697734808045"/>
+<node TEXT="KDevelop" ID="ID_439212627" CREATED="1697734808204" MODIFIED="1697734813715"/>
+</node>
+</node>
+<node TEXT="Ruby" ID="ID_244579402" CREATED="1697732665394" MODIFIED="1697732739274">
+<node TEXT="Automatización, procesamiento de datos, herramientas de linea de comandos..." ID="ID_1265035315" CREATED="1697734851819" MODIFIED="1697734890906">
+<node TEXT="RubyMine" ID="ID_1063372885" CREATED="1697734890909" MODIFIED="1697734917648"/>
+<node TEXT="Sublime Text" ID="ID_1416762088" CREATED="1697734917794" MODIFIED="1697734931200"/>
+<node TEXT="Aptana Studio" ID="ID_1473897860" CREATED="1697734931396" MODIFIED="1697734943697"/>
+</node>
+</node>
+</node>
+<node TEXT="Procedimental" ID="ID_657799444" CREATED="1697731082702" MODIFIED="1697731140809">
+<node TEXT="Pascal" ID="ID_1106692360" CREATED="1697732743976" MODIFIED="1697732774855">
+<node TEXT="Permite la programación estructurada y modularizada, se dividen los programas en módulos." ID="ID_802656190" CREATED="1697734963581" MODIFIED="1697735022934">
+<node TEXT="Lazarus" ID="ID_440751724" CREATED="1697735025301" MODIFIED="1697735063848"/>
+<node TEXT="Geany IDE" ID="ID_934917692" CREATED="1697735064028" MODIFIED="1697735077602"/>
+<node TEXT="Free Pascal" ID="ID_211437515" CREATED="1697735077772" MODIFIED="1697735083004"/>
+</node>
+</node>
+<node TEXT="Python" ID="ID_1526006484" CREATED="1697732775385" MODIFIED="1697732778623">
+<node TEXT="Ciencia de datos, machine learning, desarrollo web, procesamiento de imágenes..." ID="ID_1124987830" CREATED="1697735099615" MODIFIED="1697735134709">
+<node TEXT="PyCharm" ID="ID_578033171" CREATED="1697735136973" MODIFIED="1697735167723"/>
+<node TEXT="SlickEdit" ID="ID_1507862681" CREATED="1697735167855" MODIFIED="1697735173999"/>
+<node TEXT="Thonny" ID="ID_1886611154" CREATED="1697735174130" MODIFIED="1697735178720"/>
+</node>
+</node>
+<node TEXT="C#" ID="ID_663696714" CREATED="1697732779032" MODIFIED="1697732789224">
+<node TEXT="Aplicaciones de escritorio, servicios de API, servicios web XML..." ID="ID_1630132824" CREATED="1697735192009" MODIFIED="1697735226365">
+<node TEXT="Rider" ID="ID_1896506781" CREATED="1697735228812" MODIFIED="1697735298301"/>
+<node TEXT="MonoDevelop" ID="ID_1543480012" CREATED="1697735298458" MODIFIED="1697735303917"/>
+<node TEXT="Visual Studio Code" ID="ID_1058743797" CREATED="1697735304057" MODIFIED="1697735322497"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Declarativos" POSITION="bottom_or_right" ID="ID_615427400" CREATED="1697731023312" MODIFIED="1697731061560">
+<node TEXT="Funcional" ID="ID_1723172545" CREATED="1697731175990" MODIFIED="1697731203015">
+<node TEXT="Haskell" ID="ID_400011880" CREATED="1697732843265" MODIFIED="1697732849992">
+<node TEXT="Mapeo de algoritmos matemáticos, evaluación de expresiones, immutabilidad..." ID="ID_1577547430" CREATED="1697735363992" MODIFIED="1697735408996">
+<node TEXT="Leksah" ID="ID_823786783" CREATED="1697735408998" MODIFIED="1697735441869"/>
+<node TEXT="Intellij-Haskel" ID="ID_768947138" CREATED="1697735442039" MODIFIED="1697735462465"/>
+<node TEXT="Neovim" ID="ID_1880465942" CREATED="1697735462632" MODIFIED="1697735468517"/>
+</node>
+</node>
+<node TEXT="Miranda" ID="ID_429021671" CREATED="1697732850632" MODIFIED="1697732860275">
+<node TEXT="Permite crear simulaciones de robots en pocos clicks, se programan comportamientos" ID="ID_570323877" CREATED="1697735507197" MODIFIED="1697735558491">
+<node TEXT="Visual Studio Code" ID="ID_1111209277" CREATED="1697735562016" MODIFIED="1697735681045"/>
+<node TEXT="Emacs" ID="ID_1042573017" CREATED="1697735681212" MODIFIED="1697735684469"/>
+<node TEXT="Atom" ID="ID_1941946581" CREATED="1697735684630" MODIFIED="1697735687027"/>
+</node>
+</node>
+</node>
+<node TEXT="Lógica" ID="ID_1590875961" CREATED="1697731176729" MODIFIED="1697731197042">
+<node TEXT="Racket" ID="ID_126742410" CREATED="1697732926508" MODIFIED="1697732931044">
+<node TEXT="Posibilita la creación de nuevos lenguajes y dialectos, se usa en scripting, investigación o en la enseñanza de Ingenieria informática" ID="ID_1906399430" CREATED="1697735718644" MODIFIED="1697735789280">
+<node TEXT="DrRacket" ID="ID_543360605" CREATED="1697735789284" MODIFIED="1697735821183"/>
+<node TEXT="Emacs" ID="ID_1194281998" CREATED="1697735821352" MODIFIED="1697735835107"/>
+<node TEXT="Atom" ID="ID_1705670625" CREATED="1697735835252" MODIFIED="1697735837957"/>
+</node>
+</node>
+<node TEXT="Clojure" ID="ID_35583203" CREATED="1697732931531" MODIFIED="1697732936187">
+<node TEXT="Diseñado para centrarse en un problema sin tener que dedicar tiempo a gestionar él código repetitivo." ID="ID_936711569" CREATED="1697735857272" MODIFIED="1697735961326">
+<node TEXT="Clojure-Kit" ID="ID_542399065" CREATED="1697735961330" MODIFIED="1697735970467"/>
+<node TEXT="CIDER" ID="ID_961400212" CREATED="1697735970625" MODIFIED="1697735976271"/>
+<node TEXT="Visual Studio Code + Calva" ID="ID_1485818822" CREATED="1697735976453" MODIFIED="1697735992314"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
